96c14142a23945b0bf973698962a1845ec0a85fe
RaynerdTech <144237574+RaynerdTech@users.noreply.github.com>
2025-01-05 23:44:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization", "Accept"] // Add headers you expect
   })
 );
-
+ 
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
